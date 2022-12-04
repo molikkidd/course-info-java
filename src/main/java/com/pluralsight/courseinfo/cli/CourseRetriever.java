ac8347d80e5664827166eb8513e1035bda02368c
Molikkidd <mzkidd5@gmail.com>
2022-12-04 15:16:04 -0500

    add retrieve courses method and expection handling

--- src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -1,4 +1,25 @@
 package com.pluralsight.courseinfo.cli;
 
 public class CourseRetriever {
+
+    public static void main(String... args) {
+        System.out.println("Course Retriever started");
+
+        if(args.length == 0 ) {
+            System.out.println("Please provide an author name as first arguement.");
+            return;
+        }
+
+        try {
+            retrieveCourses(args[0]);
+        } catch(Exception e) {
+            System.out.println("Unexpected error");
+            e.printStackTrace();
+        }
+    }
+
+    private static void retrieveCourses(String authorId) {
+        System.out.println("Retrieving courses for author" + authorId);
+    }
+
 }
